e3ad16d5f3b5a9b1fa351185e9045c49aad17f71
Ionică Bizău <bizauionica@gmail.com>
2015-07-30 16:10:24 +0300

    Updated the example

--- example/index.js
+++ example/index.js
@@ -5,8 +5,33 @@ var IterateObject = require("../lib");
 IterateObject({
     name: "Bob"
   , age: 42
-}, function (name, value) {
+}, function (value, name) {
     console.log(name, value);
 });
 // => "name", "Bob"
-// => "age", 42
+//    "age", 42
+
+// Iterate an array
+IterateObject([
+    1, 2, 3, 4, 5, 6, 7
+], function (value, i) {
+    console.log("v[" + i + "] = " + value);
+});
+// => v[0] = 1
+//    v[1] = 2
+//    v[2] = 3
+//    v[3] = 4
+//    v[4] = 5
+//    v[5] = 6
+//    v[6] = 7
+
+// Iterate an array
+IterateObject([
+    "Alice", "Bob", "Carol", "Dave"
+], function (value, i, arr) {
+    console.log("Current: " + value + (arr[i + 1] ? " Next:" + arr[i + 1] : ""));
+});
+// => Current: Alice Next:Bob
+//    Current: Bob Next:Carol
+//    Current: Carol Next:Dave
+//    Current: Dave
